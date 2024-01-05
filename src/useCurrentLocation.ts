ef9c437ae6ec090f16f5b29368d0d7c3c84824c1
whoislewys <whoislewys@gmail.com>
2024-01-05 15:34:49 -0600

    clean up location hook by moving to a reducer

--- src/useCurrentLocation.ts
+++ src/useCurrentLocation.ts
@@ -1,62 +1,95 @@
 import CheapRuler from 'cheap-ruler';
-import {watchPositionAsync} from 'expo-location';
-import {useEffect, useState} from 'react';
+import {
+  LocationAccuracy,
+  LocationObject,
+  watchPositionAsync,
+} from 'expo-location';
+import {useEffect, useReducer} from 'react';
 
 // Export for unit testing and easy updating
 export const LOCATION_UPDATE_THRESHOLD_METERS = 15;
 
-const hasTwoSecondsElapsed = (
-  prevTimestamp: number,
-  currentTimestamp: number,
-): boolean => {
-  return currentTimestamp - prevTimestamp >= 2000;
-};
-
 export const useCurrentLocation = () => {
-  const [latitude, setLatitude] = useState<number>();
-  const [longitude, setLongitude] = useState<number>();
-  const [lastUpdateTimestamp, setLastUpdateTimestamp] = useState<number>();
+  const initialState: Record<string, number | undefined> = {
+    latitude: undefined,
+    longitude: undefined,
+    lastUpdateTimestamp: undefined,
+  };
+
+  const locationReducer = (
+    state: typeof initialState,
+    action: {
+      type: string;
+      latitude: number;
+      longitude: number;
+      timestamp: number;
+    },
+  ) => {
+    switch (action.type) {
+      case 'UPDATE_LOCATION':
+        return {
+          latitude: action.latitude,
+          longitude: action.longitude,
+          lastUpdateTimestamp: action.timestamp,
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(locationReducer, initialState);
 
   useEffect(() => {
     const subscribeToLocation = async () => {
-      const subscriptionPromise = watchPositionAsync({}, location => {
-        const ruler = new CheapRuler(location.coords.latitude, 'meters');
+      const subscriptionPromise = watchPositionAsync(
+        {accuracy: LocationAccuracy.High},
+        (location: LocationObject) => {
+          const ruler = new CheapRuler(location.coords.latitude, 'meters');
 
-        // Initialize state if it's undefined (e.g. if user just opened the app)
-        if (
-          lastUpdateTimestamp === undefined ||
-          latitude === undefined ||
-          longitude === undefined
-        ) {
-          setLastUpdateTimestamp(location.timestamp);
-          setLatitude(location.coords.latitude);
-          setLongitude(location.coords.longitude);
-        } else {
-          const distanceFromLastMeasurementM = ruler.distance(
-            [longitude ?? 0, latitude ?? 0],
-            [location.coords.longitude, location.coords.latitude],
-          );
-
+          // Initialize state if it's undefined (e.g. if the user just opened the app)
           if (
-            hasTwoSecondsElapsed(
-              lastUpdateTimestamp ?? 0,
-              location.timestamp,
-            ) ||
-            distanceFromLastMeasurementM > LOCATION_UPDATE_THRESHOLD_METERS
+            state.lastUpdateTimestamp === undefined ||
+            state.latitude === undefined ||
+            state.longitude === undefined
           ) {
-            setLatitude(location.coords.latitude);
-            setLongitude(location.coords.longitude);
+            dispatch({
+              type: 'UPDATE_LOCATION',
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              timestamp: location.timestamp,
+            });
+          } else {
+            const distanceFromLastMeasurementM = ruler.distance(
+              [location.coords.longitude, location.coords.latitude],
+              [state.longitude ?? 0, state.latitude ?? 0],
+            );
+
+            if (
+              location.timestamp - state.lastUpdateTimestamp >= 2000 ||
+              distanceFromLastMeasurementM > LOCATION_UPDATE_THRESHOLD_METERS
+            ) {
+              dispatch({
+                type: 'UPDATE_LOCATION',
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                timestamp: location.timestamp,
+              });
+            }
           }
-        }
-      });
+        },
+      );
+
       const subscription = await subscriptionPromise;
-      return () => subscription.remove();
+      return () => {
+        subscription.remove();
+      };
     };
+
     subscribeToLocation();
-  });
+  }, [state.lastUpdateTimestamp, state.latitude, state.longitude]);
 
   return {
-    latitude,
-    longitude,
+    latitude: state.latitude,
+    longitude: state.longitude,
   };
 };
